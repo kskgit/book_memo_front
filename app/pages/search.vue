0c549c898d72f038e7af4ae5712b11f4182d9568
keisuke <keisuke@diffeasy.com>
2020-02-13 19:47:17 +0900

    ディレクトリ構成変更

---
+++ app/pages/search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input type="text" @input="searchBooks" />
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {},
+  data() {
+    return {
+      items: []
+    }
+  },
+  computed: {},
+  methods: {
+    searchBooks() {
+      console.log(this.$api);
+      // axios.get("https://www.googleapis.com/books/v1/volumes?q=search" + query).then(response => {
+      //   console.log(response.data);
+      //   this.items = response.data.items;
+      //   });
+    }
+  },
+  created: async function() {}
+}
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 
 export default {
   mode: 'spa',
+  srcDir: 'app',
   server: {
     port: 9010,
   },
