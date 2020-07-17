364bd16f43fe149b882fd7a6b86e2b5eb9fea76b
keisuke <keisuke@diffeasy.com>
2020-07-17 22:54:43 +0900

    一覧画面作成

---
+++ app/pages/book/create/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <b-button
+      block
+      size="lg"
+      variant="primary"
+      class="text-white"
+      v-b-popover.hover.bottom="'本のバーコードから情報を読み取って登録します'"
+    >
+      <h3>バーコード読み取り</h3>
+    </b-button>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  methods:{}
+}
+</script>
+
+<style></style>
